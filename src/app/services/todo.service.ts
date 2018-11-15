50c05d05d1162ba3c980e58de1055e7eeede7ece
Jason Allen <jasonallen@Jasons-MacBook-Pro.local>
2018-11-15 16:14:18 -0600

    Inititial Commit

--- src/app/services/todo.service.ts
+++ src/app/services/todo.service.ts
@@ -6,34 +6,41 @@ import { Todo } from '../classes/todo';
 })
 export class TodoService {
 
-	private todos: Todo[];
 	private nextId: number;
 
 	constructor() {
-		this.todos = [
-		new Todo(0, "Wake up"),
-		new Todo(1, "Get out of bed"),
-		new Todo(2, "Make bed"),
-		new Todo(3, "Brush teeth"),
-		new Todo(4, "Get dressed"),
-		new Todo(5, "Grab books"),
-		new Todo(6, "Go to class")
-		];
+		let todos = this.getTodos();
 
-		this.nextId = 7;
+		if (todos.length == 0) {
+			this.nextId = 0;
+		} else {
+				let maxId = todos[todos.length - 1].id;
+				this.nextId = maxId + 1;
+			}
+		
 	 }
 
 	 public addTodo(text: string): void {
 	 	let todo = new Todo(this.nextId, text);
-	 	this.todos.push(todo);
+	 	let todos = this.getTodos();
+	 	todos.push(todo);
+
+	 	this.setLocalStorageTodos(todos);
 	 	this.nextId++;
 	 }
 
 	 public getTodos(): Todo[] {
-	 	return this.todos;
+	 	let localStorageItem = JSON.parse(localStorage.getItem('todos'));
+	 	return localStorageItem == null ? [] : localStorageItem.todos;
 	 }
 
 	 public removeTodo(id: number): void {
-	 	this.todos = this.todos.filter((todo) => todo.id != id);
+	 	let todos = this.getTodos();
+	 	todos = todos.filter((todo) => todo.id != id);
+	 	this.setLocalStorageTodos(todos);
+	 }
+
+	 private setLocalStorageTodos(todos: Todo[]): void {
+	 	localStorage.setItem('todos', JSON.stringify({ todos: todos }));
 	 }
 }
